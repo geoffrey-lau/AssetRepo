fda278c5a5306dfc8b5bff7e8f41bde5f11b1410
geoffrey-lau <40555084+geoffrey-lau@users.noreply.github.com>
2019-09-18 16:23:31 +0200

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-sd7r8:fqnrdxbxr2tks4rdp92qxnbwrwzgddgkc9x9tx5cmvwkfh47k2sxgb
+./rancher login https://k8s.eur.ad.sag/v3 --token token-8jssh:xb6jrjtm4nxb2lhqbv47hmqcxczdpfvkxcmd47knv684hhcjxgnwrx
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
